e79db71e1bb6a7ed36d7e65dbb72fbcb715dbf80
hiren mistry <hiren.mistry@chai-monsters.com>
2012-04-15 19:03:26 -0700

    Initial Commit

---
+++ TiledScrollViewDebug/TiledScrollViewDebugAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TiledScrollViewDebugAppDelegate.h
+//  TiledScrollViewDebug
+//
+//  Created by Hiren on 4/15/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TiledScrollViewDebugAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
